0df0a0cce80a71127cfcb88c83f437d6704cf0fb
Timofei Irishiin <eblan2204.1@gmail.com>
2026-09-25 21:14:47 +0300

    Добавлена карточка UI перевода и интеграция в приложение

---
+++ src/PaymentApp.UI/Views/TransferView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PaymentApp.UI.Views
+{
+    public partial class TransferView : UserControl
+    {
+        public TransferView()
+        {
+            InitializeComponent();
+        }
+    }
+}
