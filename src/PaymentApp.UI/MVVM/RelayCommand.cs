5816bf814bf8ace027648296eb5c7b0b6a3e17ab
nevollwar <tverskojnikita4@gmail.com>
2026-09-25 19:21:45 +0300

    Добавлены базовые классы MVVM и каркас операций

---
+++ src/PaymentApp.UI/MVVM/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PaymentApp.UI.Mvvm
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> execute;
+        private readonly Predicate<object?>? canExecute;
+
+        public RelayCommand(Action<object?> executeAction, Predicate<object?>? canExecutePredicate = null)
+        {
+            execute = executeAction ?? throw new ArgumentNullException(nameof(executeAction));
+            canExecute = canExecutePredicate;
+        }
+
+        public RelayCommand(Action executeAction, Func<bool>? canExecuteFunc = null)
+            : this(_ => executeAction(), canExecuteFunc != null ? _ => canExecuteFunc() : null)
+        {
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ src/PaymentApp.UI/ViewModels/OperationViewModelBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Input;
+using PaymentApp.Domain;
+using PaymentApp.UI.Mvvm;
+
+namespace PaymentApp.UI.ViewModels
+{
+    public abstract class OperationViewModelBase : ViewModelBase
+    {
+        public abstract string Title { get; }
+        public abstract ContractInfo Contract { get; }
+
+        private bool isPreValid;
+        public bool IsPreValid
+        {
+            get => isPreValid;
+            set => SetField(ref isPreValid, value);
+        }
+
+        private bool? isPostValid;
+        public bool? IsPostValid
+        {
+            get => isPostValid;
+            set => SetField(ref isPostValid, value);
+        }
+
+        private string statusMessage = "Заполните параметры операции";
+        public string StatusMessage
+        {
+            get => statusMessage;
+            set => SetField(ref statusMessage, value);
+        }
+
+        public ICommand ExecuteCommand { get; }
+
+        protected OperationViewModelBase()
+        {
+            ExecuteCommand = new RelayCommand(RunOperation, () => IsPreValid);
+        }
+
+        // Челы 2, 3, 4 будут переопределять эти два метода под свои операции
+        public abstract void CheckPreconditions();
+        public abstract void Execute();
+
+        private void RunOperation()
+        {
+            CheckPreconditions();
+            if (!IsPreValid)
+            {
+                return;
+            }
+
+            try
+            {
+                Execute();
+                IsPostValid = true;
+                StatusMessage = "Успешно: операция выполнена, постусловия истинны.";
+            }
+            catch (Exception ex)
+            {
+                IsPostValid = false;
+                StatusMessage = "Ошибка выполнения: " + ex.Message;
+            }
+        }
+    }
+}
