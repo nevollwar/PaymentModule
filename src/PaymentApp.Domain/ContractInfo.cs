2fd003dda2bf43dd3203058ae04e399adbc28a29
nevollwar <tverskojnikita4@gmail.com>
2026-09-25 19:15:12 +0300

    создана структура проекта, а также добавлены классы Guard и ContractInfo в Domain

---
+++ src/PaymentApp.Domain/ContractInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace PaymentApp.Domain
+{
+    /// <summary>
+    /// Модель данных для модального окна «Показать контракт»
+    /// </summary>
+    public record ContractInfo(
+        string OperationName,
+        string Preconditions,
+        string Postconditions,
+        string EffectsAndExceptions,
+        string ValidExample,
+        string InvalidExample
+        );
+}
---
+++ src/PaymentApp.Domain/Guard.cs
@@ -0,0 +1,21 @@
+﻿namespace PaymentApp.Domain
+{
+    /// <summary>
+    /// Класс для проверки контрактов (предусловий)
+    /// </summary>
+    public static class Guard
+    {
+        public static void Requires(bool condition, string message = "Нарушено предусловие")
+        {
+            if (!condition)
+            {
+                throw new PreconditionException(message);
+            }
+        }
+    }
+
+    public class PreconditionException: Exception
+    {
+        public PreconditionException(string message) : base(message) { }
+    }
+}
